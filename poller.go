4c0fc7ff869c3dd1cbc626608abe5931c5e18913
Alexseij <79832610507@yandex.ru>
2021-07-21 20:32:09 +0300

    Rework poller for new api 🔨

--- poller.go
+++ poller.go
@@ -1,10 +1,13 @@
 package goteleg
 
-import "log"
+import (
+	"log"
+)
 
 type longPoller struct {
 	offset         int
 	timeout        int
+	limit          int
 	allowedUpdates []string
 	updates        chan *Update
 }
@@ -16,12 +19,23 @@ type LongPollerSettings struct {
 }
 
 func (lp *longPoller) startPolling(api API) {
+
+	options := map[string]interface{}{
+		"offset":  lp.offset,
+		"timeout": lp.timeout,
+		"limit":   lp.limit,
+	}
+
+	offset := lp.offset
+
 	for {
-		updates, err := api.GetUpdates(lp.offset+1, lp.timeout)
+		updates, err := api.GetUpdates(options)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
+		offset++
+		options["offset"] = offset
 		for _, update := range updates.Result {
 			lp.offset = update.UpdateID
 			lp.updates <- update
@@ -31,6 +45,7 @@ func (lp *longPoller) startPolling(api API) {
 
 func newLongPoller(setting LongPollerSettings) *longPoller {
 	return &longPoller{
+		limit:          setting.Limit,
 		timeout:        setting.Timeout,
 		allowedUpdates: setting.AllowedUpdates,
 		updates:        make(chan *Update, setting.Limit),
